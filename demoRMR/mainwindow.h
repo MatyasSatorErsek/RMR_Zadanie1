c293196d802136a9ffc08ec202cd330dce233d09
unknown <satormatyi@gmail.com>
2024-02-21 20:58:53 +0100

    First

---
+++ demoRMR/mainwindow.h
@@ -0,0 +1,94 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTimer>
+#ifdef _WIN32
+#include<windows.h>
+#endif
+#include<iostream>
+//#include<arpa/inet.h>
+//#include<unistd.h>
+//#include<sys/socket.h>
+#include<sys/types.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<vector>
+//#include "ckobuki.h"
+//#include "rplidar.h"
+
+#include "robot.h"
+#define TICK_TO_METER 0.000085292090497737556558
+
+namespace Ui {
+class MainWindow;
+}
+
+///toto je trieda s oknom.. ktora sa spusti ked sa spusti aplikacia.. su tu vsetky gombiky a spustania...
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+    int processThisLidar(LaserMeasurement laserData);
+
+    int processThisRobot(TKobukiData robotdata);
+
+
+
+private slots:
+    void on_pushButton_9_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_clicked();
+    void getNewFrame();
+
+private:
+
+    //--skuste tu nic nevymazat... pridavajte co chcete, ale pri odoberani by sa mohol stat nejaky drobny problem, co bude vyhadzovat chyby
+    Ui::MainWindow *ui;
+     void paintEvent(QPaintEvent *event);// Q_DECL_OVERRIDE;
+     int updateLaserPicture;
+     LaserMeasurement copyOfLaserData;
+     std::string ipaddress;
+     Robot robot;
+     TKobukiData robotdata;
+     int datacounter;
+     QTimer *timer;
+
+     double distance;
+     double x;
+     double y;
+     double phi;
+
+     double encRight;
+     double encLeft;
+     double gyro;
+
+     void getOdometry(TKobukiData robotdata);
+
+     double forwardspeed;//mm/s
+     double rotationspeed;//omega/s
+public slots:
+     void setUiValues(double robotX,double robotY,double robotFi);
+signals:
+     void uiValuesChanged(double newrobotX,double newrobotY,double newrobotFi); ///toto nema telo
+
+
+};
+
+#endif // MAINWINDOW_H
